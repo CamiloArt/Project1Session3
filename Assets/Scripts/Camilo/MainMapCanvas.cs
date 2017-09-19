c8e782ece70163597e3e9725a6f78c483b737ffc
Unknown <camilocastro316@gmail.com>
2017-09-19 19:14:52 -0400

    update scene

---
+++ Assets/Scripts/Camilo/MainMapCanvas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMapCanvas : MonoBehaviour {
+	public Text counter;
+	private GameEngine gameEngine;
+	// Use this for initialization
+	void Start () {
+		gameEngine = GameObject.FindGameObjectWithTag ("GameEngine").GetComponent<GameEngine> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter.text = Mathf.Round(gameEngine.timeCounter).ToString();
+	}
+}
