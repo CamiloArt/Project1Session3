309b7d03f52b915abea729382084bab42203ed2b
Nino <ninosin@hotmail.com>
2017-09-20 19:39:27 -0400

    Nomral Controller

---
+++ Assets/Scripts/Nino/NormalController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalController : MonoBehaviour {
+	
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+
+		float x = Input.GetAxis ("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+
+		transform.Translate (x * Time.deltaTime * speed,0,y * Time.deltaTime * speed, Space.World);
+
+		float angle = Mathf.Atan2 (x, y);
+
+		transform.rotation = Quaternion.EulerAngles (0, angle, 0);
+	
+	}
+}
