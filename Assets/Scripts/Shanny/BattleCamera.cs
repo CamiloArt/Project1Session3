3dd32ee7fb8494afa8ba4fd5a366b4cf0dd0d33e
Shagmeister <shagaocama08@hotmail.com>
2017-10-14 20:45:56 -0400

    Started the Battle Camera.

---
+++ Assets/Scripts/Shanny/BattleCamera.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleCamera : MonoBehaviour 
+{
+    private GameEngine gameEngine;
+    private Vector3 Player01, Player02;
+	
+	void Start () 
+    {
+		gameEngine = GameObject.FindGameObjectWithTag("GameEngine").GetComponent<GameEngine>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        if (gameEngine.gameState == "battlemap")
+        {
+            if (!gameEngine.inBattle)
+            {
+                PlayerTarget();
+            }
+            else
+            {
+                DynamicCamera();
+            }
+        }
+	}
+
+    void PlayerTarget()
+    {
+        Player01 = gameEngine.players[gameEngine.currentPlayerIndex].gameObject.transform.position;
+        Player02 = gameEngine.players[gameEngine.playerInRangeIndex].gameObject.transform.position;
+    }
+
+    void DynamicCamera()
+    {
+    Vector3 midPoint= (Player01 + Player02) / 2;
+    }
+}
+
