8f0f7538a36c66bff26f1fad141dc3af229e4865
Abe <abers1997@gmail.com>
2017-10-26 02:07:56 -0400

    Added current player Level text

---
+++ Assets/Scripts/Abe/BattleMap/ConsumableBattleUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsumableBattleUI : MonoBehaviour {
+
+    public GameEngine gameEngine;
+
+    public GameObject BattleUI;
+
+    void Start()
+    {
+        BattleUI.SetActive(false);
+    }
+
+	void Update() 
+    {
+        if (gameEngine.gameState == "battlemap")
+        {
+            BattleUI.SetActive(true);
+        }
+        else
+        {
+            BattleUI.SetActive(false);
+        }
+	}
+}
--- Assets/Scripts/Abe/StartMenu/ButtonHighlight.cs
+++ Assets/Scripts/Abe/StartMenu/ButtonHighlight.cs
@@ -13,10 +13,13 @@ public class ButtonHighlight : MonoBehaviour, IPointerEnterHandler, IPointerExit
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
+        transform.localScale += new Vector3(0.7f, 0.7f, 0.7f);
+        Debug.Log("Run");
     }
     public void OnPointerExit(PointerEventData eventData)
     {
-        
+        transform.localScale = initialScale;
+        Debug.Log("Exit");
     }
     public void OnSelect(BaseEventData eventData)
     {
